3a9f06cb7971a4a333d6b536cc4165584b88889a
danielntll <116676924+danielntll@users.noreply.github.com>
2023-08-27 22:11:20 +0200

    Feat: getAll completed Todos

--- src/components/Item__TodosTatistics/ItemTodosTatistics.tsx
+++ src/components/Item__TodosTatistics/ItemTodosTatistics.tsx
@@ -1,6 +1,8 @@
 import styles from "./ItemTodosTatistics.module.css";
 import { IonBadge, IonButton, IonIcon, IonItem, IonLabel } from "@ionic/react";
-import { calendarNumberOutline, chevronExpand } from "ionicons/icons";
+import { chevronExpand } from "ionicons/icons";
+import { useState } from "react";
+import ModalCompletedTodos from "../Modal__CompletedTodos/ModalCompletedTodos";
 
 interface ContainerProps {
   title: string;
@@ -11,29 +13,42 @@ interface ContainerProps {
 const ItemTodosTatistics: React.FC<ContainerProps> = ({ dailyCompleted, title, icon }) => {
   // VARIABLES ---------------------
   // CONDITIONS --------------------
+  const [isOpen, setIsOpen] = useState(false);
   // FUNCTIONS ---------------------
+  const handleOpenModal = () => {
+    setIsOpen(true);
+  }
   // RETURN ------------------------
-  return <IonItem className={styles.ItemTodosTatistics}>
+  return (
+    <>
+      <IonItem className={styles.ItemTodosTatistics}>
+        <IonIcon
+          color="primary"
+          slot="start"
+          icon={icon}
+        />
+        <IonLabel>
+          <p className={styles.ItemTodosTatistics__inline}>{title}
+            <IonBadge>
+              <span className={styles.ItemTodosTatistics__number}>
+                {dailyCompleted ? dailyCompleted.length : 0}
+              </span>
+            </IonBadge>
+          </p>
+        </IonLabel>
+        <IonButton
+          onClick={() => handleOpenModal()}
+          slot="end">
+          <IonIcon icon={chevronExpand} />
+        </IonButton>
+      </IonItem>
 
-    <IonIcon
-      color="primary"
-      slot="start"
-      icon={icon}
-    />
-
-    <IonLabel>
-      <p className={styles.ItemTodosTatistics__inline}>{title}
-        <IonBadge>
-          <span className={styles.ItemTodosTatistics__number}>
-            {dailyCompleted ? dailyCompleted.length : 0}
-          </span>
-        </IonBadge>
-      </p>
-    </IonLabel>
-    <IonButton slot="end">
-      <IonIcon icon={chevronExpand} />
-    </IonButton>
-  </IonItem>;
+      <ModalCompletedTodos
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        dailyCompleted={dailyCompleted}
+      />
+    </>);
 };
 
 export default ItemTodosTatistics;
--- src/components/Modal__CompletedTodos/ModalCompletedTodos.tsx
+++ src/components/Modal__CompletedTodos/ModalCompletedTodos.tsx
@@ -1,42 +1,89 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { typeAviableLanguages } from "../../types/typeAviableLanguages";
 import { LanguageContext } from "../../utils/reducers/reducerLanguage";
 import styles from "./ModalCompletedTodos.module.css";
 import { text } from "./text";
-import { IonButton, IonButtons, IonHeader, IonModal, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButton, IonButtons, IonContent, IonHeader, IonModal, IonTitle, IonToolbar } from "@ionic/react";
+import { typeTodo } from "../../types/typeTodo";
+import { firebaseTodoActions } from "../../firebase/firebaseTodoActions";
+import { useAuthContext } from "../../firebase/auth";
+import ItemUserTodo from "../Item__UserTodo/ItemUserTodo";
 
 interface ContainerProps {
   isOpen: boolean,
   setIsOpen: (value: boolean) => void;
+  dailyCompleted: string[];
 }
 
 const ModalCompletedTodos: React.FC<ContainerProps> = (
   {
     isOpen,
-    setIsOpen
+    setIsOpen,
+    dailyCompleted,
   }
 ) => {
   // VARIABLES ---------------------
+  const { userUID } = useAuthContext();
   const { stateLanguage, dispatchLanguage } = useContext(LanguageContext);
   const language: typeAviableLanguages = stateLanguage;
   // CONDITIONS --------------------
+  const [completedTodos, setCompletedTodos] = useState<any[]>([]);
   // FUNCTIONS ---------------------
-  // RETURN ------------------------
-  return <IonModal className={styles.container}>
-    <IonHeader>
-      <IonToolbar>
-        <IonButtons slot="start">
-          <IonButton>
-            {text[language].closeModal}
-          </IonButton>
-        </IonButtons>
-        <IonTitle>
-          {text[language].title}
-        </IonTitle>
-      </IonToolbar>
-    </IonHeader>
+  useEffect(() => {
+    if (isOpen) {
+      handleGetData()
+    }
+  }, [isOpen]);
 
-  </IonModal>;
+  const handleGetData = async () => {
+    const auxCompletedTodos: any[] = [];
+    const promiseCompleted = dailyCompleted.map(async (UID: string) => {
+      auxCompletedTodos.push(await firebaseTodoActions.READ(userUID!, UID));
+    });
+    Promise.all(promiseCompleted).then(() => {
+      setCompletedTodos(auxCompletedTodos);
+    })
+  }
+
+  const handleCallback = () => {
+    console.log("handleCallback")
+  }
+
+  useEffect(() => {
+    console.log(completedTodos);
+  }, [completedTodos])
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  }
+  // RETURN ------------------------
+  return (
+    <IonModal
+      isOpen={isOpen}
+      className={styles.container}>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonButton onClick={() => handleCloseModal()}>
+              {text[language].closeModal}
+            </IonButton>
+          </IonButtons>
+          <IonTitle>
+            {text[language].title}
+          </IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        {completedTodos.map((todo: typeTodo, index: number) => {
+          return (
+            <ItemUserTodo
+              callback={handleCallback}
+              todo={todo}
+              key={todo.todoUID + index} />
+          )
+        })}
+      </IonContent>
+    </IonModal>);
 };
 
 export default ModalCompletedTodos;
